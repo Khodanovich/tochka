8e508ff2cf44c0b33232d8c065c953723af1fdc7
Alexsandr <alex-migon@mail.ru>
2019-09-08 17:02:46 +0500

    Fix multidex. Fix keys.

--- app/src/main/java/com/github/khodanovich/tochka/features/global/error/ErrorHandlerImpl.kt
+++ app/src/main/java/com/github/khodanovich/tochka/features/global/error/ErrorHandlerImpl.kt
@@ -1,6 +1,7 @@
 package com.github.khodanovich.tochka.features.global.error
 
 import androidx.lifecycle.MutableLiveData
+import retrofit2.HttpException
 
 interface ErrorHandler{
     fun proceed(errorMessagesLiveData: MutableLiveData<String>, throwable: Throwable)
@@ -8,12 +9,34 @@ interface ErrorHandler{
 
 class ErrorHandlerImpl : ErrorHandler {
 
+    companion object {
+        private const val FORBIDDEN_CODE = 403
+        private const val FORBIDDEN_MESSAGE = "Исчерпано количество запросов для неавторизованного пользователя"
+        private const val DEFAULT_MESSAGE = "Что то пошло не так, мы уже работаем над этим"
+    }
+
     override fun proceed(errorMessagesLiveData: MutableLiveData<String>, throwable: Throwable) {
         processException(errorMessagesLiveData, throwable)
     }
 
     private fun processException(errorMessagesLiveData: MutableLiveData<String>, throwable: Throwable){
-        errorMessagesLiveData.postValue("Что то пошло не так, мы уже работаем над этим")
-        //Тут необходимо обработать ошибки
+        when(throwable) {
+            is HttpException -> processHttpException(errorMessagesLiveData, throwable)
+            else -> showDefaultMessage(errorMessagesLiveData)
+
+        }
     }
+
+    private fun processHttpException(errorMessagesLiveData: MutableLiveData<String>, httpException: HttpException){
+        when(httpException.code()){
+            FORBIDDEN_CODE -> errorMessagesLiveData.postValue(FORBIDDEN_MESSAGE)
+            else -> showDefaultMessage(errorMessagesLiveData)
+        }
+    }
+
+    private fun showDefaultMessage(errorMessagesLiveData: MutableLiveData<String>){
+        errorMessagesLiveData.postValue(DEFAULT_MESSAGE)
+    }
+
+
 }
